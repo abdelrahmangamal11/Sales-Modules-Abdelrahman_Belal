314d517fb53f92993574bb943cd5f04b2f73a661
AbdelrahmanMo111 <abdo.aboerba@gmail.com>
2025-11-24 03:35:50 +0200

    add Base Model

---
+++ base.py
@@ -0,0 +1,32 @@
+from abc import ABC
+
+
+class BaseModel(ABC):
+    """
+    Very simplified base model (similar idea to odoo.models.Model):
+    - gives each record an auto-increment ID
+    - stores a name
+    """
+    _next_id = 1
+
+    def __init__(self, name):
+        self._id = BaseModel._next_id
+        BaseModel._next_id += 1
+        self._name = name
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        self._name = value
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, name={self.name})"
